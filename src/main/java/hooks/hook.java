fe97b581e6a6e847e111614bb275ebe44714ea6c
cibi-sudo <cibiccf437@gmail.com>
2023-08-04 15:21:45 +0530

    Added parallel_execution

--- src/main/java/hooks/hook.java
+++ src/main/java/hooks/hook.java
@@ -5,15 +5,19 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
+import utility.configReader;
 import utility.helper;
+
+import java.util.Properties;
 
 public class hook {
     WebDriver driver;
 
     @Before
     public void setup() {
-        driver = browser.initializeBrowser("chrome");
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Properties prop = new configReader().intializeProperties();
+        driver = browser.initializeBrowser(prop.getProperty("browser"));
+        driver.get(prop.getProperty("url"));
     }
 
     @After
